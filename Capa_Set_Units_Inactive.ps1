ef4ae21a7b882d4da88e0865c22797a066430e41
ITCMARA <56847489+Mark5900@users.noreply.github.com>
2023-06-20 20:44:57 +0200

    Update Capa_Set_Units_Inactive.ps1
    Added powerpack script parts

--- Capa_Set_Units_Inactive.ps1
+++ Capa_Set_Units_Inactive.ps1
@@ -17,14 +17,16 @@ Import-Module Mark.Boennelykke
 ##################
 # DO NOT CHANGE
 $ScriptName = 'Capa_Set_Units_Inactive'
-$Global:ScriptFailed = 0
+$Script:ScriptFailed = 0
 # Change as needed
 $CapaServer = 'CISRVKURSUS'
 $Database = 'CapaInstaller'
 $DefaultManagementPointDev = '1'
 $DefaultManagementPointProd = $null #Keep null if you don't have two enviroments
-$Global:LastRunDate = 90
-$Global:InactiveFolderName = "Inaktiv i mindst $Global:LastRunDate dage"
+$Script:LastRunDate = 90
+
+$Script:InactiveFolderStructure = "Inaktiv i mindst $Script:LastRunDate dage"
+$Script:InactiveActiveFolderStructure = "$Script:InactiveFolderStructure\Aktive igen" # If $null nothing happens else will move the units to the folder structure spiciefied
 
 #################
 ### FUNCTIONS ###
@@ -33,10 +35,18 @@ function Set-UnitInactive {
     param (
         $oCMS
     )
-    $Date = (Get-Date).AddDays(-$Global:LastRunDate)
+    $ScriptPart = 'Set-UnitInactive'
+    $Date = (Get-Date).AddDays(-$Script:LastRunDate)
 
     $AllUnits = Get-CapaUnits -CapaSDK $oCMS -Type Computer
     foreach ($Unit in $AllUnits) {
+        # Units to skip
+        if ($Unit.IsMobileDevice -eq $true) {
+            # Skip mobile devices
+            continue
+        }
+
+        # Variables
         $LastRunTime = (Get-CapaUnitLastRuntime -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer).Split(' ')
         $LastRunTime = $LastRunTime[0].Split('-')
         $LastRunTime = Get-Date -Day $LastRunTime[0] -Month $LastRunTime[1] -Year $LastRunTime[2]
@@ -44,11 +54,31 @@ function Set-UnitInactive {
         $UnitLastExecuted = $Unit.LastExecuted.Split(' ')
         $UnitLastExecuted = $UnitLastExecuted[0].Split('-')
         $UnitLastExecuted = Get-Date -Day $UnitLastExecuted[0] -Month $UnitLastExecuted[1] -Year $UnitLastExecuted[2]
+
+        $UnitFolder = Get-CapaUnitFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer
         
-        if ($LastRunTime -lt $Date -or $UnitLastExecuted -lt $Date) {
-            Write-MBLogLine "Unit to set inactive: $($Unit.Name) - LastRunTime: $LastRunTime - UnitLastExecuted: $UnitLastExecuted"
-            Set-CapaUnitStatus -CapaSDK $oCMS -UnitName $Unit.Name -Status Inactive | Out-Null
-            Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer -FolderStructure $Global:InactiveFolderName -CreateFolder true | Out-Null
+        # Code
+        if ($UnitLastExecuted -lt $Date) {
+            Write-MBLogLine -ScriptPart $ScriptPart -Text "Unit to set inactive: $($Unit.Name) - LastRunTime: $LastRunTime - UnitLastExecuted: $UnitLastExecuted"
+            [bool]$Status = Set-CapaUnitStatus -CapaSDK $oCMS -UnitName $Unit.Name -Status Inactive
+            if ($Status -ne $true) {
+                Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to set unit $($Unit.Name) to inactive" -ForegroundColor Red
+                $Script:ScriptFailed = 1
+            }
+
+            if ($UnitFolder -eq "$Script:InactiveFolderStructure\") {
+                [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer -FolderStructure $Script:InactiveFolderStructure -CreateFolder true
+                if ($Status -ne $true) {
+                    Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveFolderStructure" -ForegroundColor Red
+                    $Script:ScriptFailed = 1
+                }
+            }
+        } elseif ($null -ne $Script:InactiveActiveFolderStructure -and $UnitFolder -eq "$Script:InactiveFolderStructure\" -and $Unit.Status -eq 'Active') {
+            [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer -FolderStructure $Script:InactiveActiveFolderStructure -CreateFolder true
+            if ($Status -ne $true) {
+                Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveActiveFolderStructure" -ForegroundColor Red
+                $Script:ScriptFailed = 1
+            }
         }
     }
 }
@@ -59,21 +89,20 @@ function Set-UnitInactive {
 
 Start-MBScriptLogging -Path $PSScriptRoot -LogName $ScriptName -DeleteDaysOldLogs 30
 
-try{
-If ($null -eq $DefaultManagementPointProd){
-    $oCMSDev = Initialize-CapaSDK -Server $CapaServer -Database $Database
-    $oCMSProd = $oCMSDev
-}
-else{
-    $oCMSDev = Initialize-CapaSDK -Server $CapaServer -Database $Database -DefaultManagementPoint $DefaultManagementPointDev
-    $oCMSProd = Initialize-CapaSDK -Server $CapaServer -Database $Database -DefaultManagementPoint $DefaultManagementPointProd
-}
-}catch {
+try {
+    If ($null -eq $DefaultManagementPointProd) {
+        $oCMSDev = Initialize-CapaSDK -Server $CapaServer -Database $Database
+        $oCMSProd = $oCMSDev
+    } else {
+        $oCMSDev = Initialize-CapaSDK -Server $CapaServer -Database $Database -DefaultManagementPoint $DefaultManagementPointDev
+        $oCMSProd = Initialize-CapaSDK -Server $CapaServer -Database $Database -DefaultManagementPoint $DefaultManagementPointProd
+    }
+} catch {
     $Error[0]
-    $Global:ScriptFailed = 1
+    $Script:ScriptFailed = 1
 }
 
-If ($Global:ScriptFailed -ne 0) {
+If ($Script:ScriptFailed -ne 0) {
     $Text = "Script $ScriptName failed, check the log for more information on server $($env:COMPUTERNAME)"
     if ($Test -ne $true) {
         #TODO: Send mail with error message
