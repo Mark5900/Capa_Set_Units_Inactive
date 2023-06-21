e076fccbb16784f19fa8b3dc3c9475ed0d4a0bb3
ITCMARA <56847489+Mark5900@users.noreply.github.com>
2023-06-21 11:14:15 +0200

    backup

--- ServerTask_Capa_Set_Units_Inactive.ps1
+++ ServerTask_Capa_Set_Units_Inactive.ps1
@@ -1,28 +1,22 @@
-Import-Module Capa.PowerShell.Module.SDK.Authentication
-Import-Module Capa.PowerShell.Module.SDK.Unit
-Import-Module Mark.Boennelykke
-<#
-    .NOTES
-    ===========================================================================
-    Created with:	Visual Studio Code
-    Created on:     20-06-2023
-    Created by:		Mark5900
-    Organization:	
-    Filename:       Capa_Set_Units_Inactive.ps1
-    ===========================================================================
-    .DESCRIPTION
-        This script will set units inactive if they have not been executed for a given amount of days.
-        If desired units that have become active again will be moved to a folder structure.
-        Likewise units that have not been executed for a given amount of days but have an run time newer than the given amount of days will be moved to a folder structure,
-        this will also apply to units where the name is reused (a good bug).
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [string]$Packageroot,
+    [Parameter(Mandatory = $true)]
+    [string]$AppName,
+    [Parameter(Mandatory = $true)]
+    [string]$AppRelease,
+    [Parameter(Mandatory = $true)]
+    [string]$LogFile,
+    [Parameter(Mandatory = $true)]
+    [string]$TempFolder,
+    [Parameter(Mandatory = $true)]
+    [string]$DllPath,
+    [Parameter(Mandatory = $false)]
+    [Object]$InputObject = $null
+)
 
-        REMEMBER TO CHANGE THE PARAMETERS BELOW AND LOOK AT THE TODO'S
-#>
-##################
-### PARAMETERS ###
-##################
 # DO NOT CHANGE
-$ScriptName = 'Capa_Set_Units_Inactive'
 $Script:ScriptFailed = 0
 # Change as needed
 $CapaServer = 'CISRVKURSUS'
@@ -35,14 +29,12 @@ $Script:InactiveFolderStructure = "Inaktiv i mindst $Script:LastRunDate dage"
 $Script:InactiveActiveFolderStructure = "$Script:InactiveFolderStructure\Aktive igen" # If $null nothing happens else will move the units to the folder structure spiciefied
 $Script:InactiveLastRunTimeFolderStructure = "$Script:InactiveFolderStructure\Bad agent" # If $null nothing happens else will move the units to the folder structure spiciefied
 
-#################
-### FUNCTIONS ###
-#################
 function Set-UnitInactive {
     param (
         $oCMS
     )
-    $ScriptPart = 'Set-UnitInactive'
+    $cs.Log_SectionHeader('Set-UnitInactive', 'o')
+
     $InactiveCount = 0
     $InactiveActiveCount = 0
     $InactiveLastRunTimeCount = 0
@@ -71,31 +63,33 @@ function Set-UnitInactive {
 
         #region Code
         if ($null -ne $Script:InactiveLastRunTimeFolderStructure -and $UnitLastExecuted -lt $Date -and $LastRunTime -gt $Date) {
-            Write-MBLogLine -ScriptPart $ScriptPart -Text "The unit $($Unit.Name) has not been executed for $Script:LastRunDate days but the last run time is $LastRunTime" -ForegroundColor Yellow
+            $cs.Job_WriteLog("The unit $($Unit.Name) has not been executed for $Script:LastRunDate days but the last run time is $LastRunTime")
 
             if ($Test -ne $true) {
                 [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer -FolderStructure $Script:InactiveLastRunTimeFolderStructure -CreateFolder true
                 if ($Status -ne $true) {
-                    Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveLastRunTimeFolderStructure" -ForegroundColor Red
+                    $cs.Job_WriteLog("Failed to add unit $($Unit.Name) to folder $Script:InactiveLastRunTimeFolderStructure")
+                    
                     $Script:ScriptFailed = 1
                 }
             }
 
             $InactiveLastRunTimeCount++
         } elseif ($UnitLastExecuted -lt $Date) {
-            Write-MBLogLine -ScriptPart $ScriptPart -Text "Unit to set inactive: $($Unit.Name) - LastRunTime: $LastRunTime - UnitLastExecuted: $UnitLastExecuted" -ForegroundColor Red
+            $cs.Job_WriteLog("Unit to set inactive: $($Unit.Name) - LastRunTime: $LastRunTime - UnitLastExecuted: $UnitLastExecuted")
 
             if ($Test -ne $true) {
                 [bool]$Status = Set-CapaUnitStatus -CapaSDK $oCMS -UnitName $Unit.Name -Status Inactive
                 if ($Status -ne $true) {
-                    Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to set unit $($Unit.Name) to inactive" -ForegroundColor Red
+                    $cs.Job_WriteLog("Failed to set unit $($Unit.Name) to inactive")
+
                     $Script:ScriptFailed = 1
                 }
 
                 if ($UnitFolder -ne "$Script:InactiveFolderStructure\") {
                     [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer -FolderStructure $Script:InactiveFolderStructure -CreateFolder true
                     if ($Status -ne $true) {
-                        Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveFolderStructure" -ForegroundColor Red
+                        $cs.Job_WriteLog("Failed to add unit $($Unit.Name) to folder $Script:InactiveFolderStructure")
                         $Script:ScriptFailed = 1
                     }
                 }
@@ -103,12 +97,12 @@ function Set-UnitInactive {
 
             $InactiveCount++
         } elseif ($null -ne $Script:InactiveActiveFolderStructure -and $UnitFolder -eq "$Script:InactiveFolderStructure\" -and $Unit.Status -eq 'Active') {
-            Write-MBLogLine -ScriptPart $ScriptPart -Text "The unit $($Unit.Name) has become active again" -ForegroundColor Magenta
+            $cs.Job_WriteLog("The unit $($Unit.Name) has become active again")
 
             if ($Test -ne $true) {
                 [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer -FolderStructure $Script:InactiveActiveFolderStructure -CreateFolder true
                 if ($Status -ne $true) {
-                    Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveActiveFolderStructure" -ForegroundColor Red
+                    $cs.Job_WriteLog("Failed to add unit $($Unit.Name) to folder $Script:InactiveActiveFolderStructure")
                     $Script:ScriptFailed = 1
                 }
             }
@@ -118,29 +112,58 @@ function Set-UnitInactive {
         #endregion
     }
 
-    Write-MBLogLine -ScriptPart $ScriptPart -Text "Inactive: $InactiveCount - InactiveActive: $InactiveActiveCount - InactiveLastRunTime: $InactiveLastRunTimeCount"
+    $cs.Job_WriteLog("Inactive: $InactiveCount - InactiveActive: $InactiveActiveCount - InactiveLastRunTime: $InactiveLastRunTimeCount")
 }
-
-##############
-### SCRIPT ###
-##############
-
-Start-MBScriptLogging -Path $PSScriptRoot -LogName $ScriptName -DeleteDaysOldLogs 30
 
 try {
+    ### Download package kit
+    [bool]$global:DownloadPackage = $true
+
+    ##############################################
+    #load core PS lib - don't mess with this!
+    if ($InputObject) { $pgkit = '' }else { $pgkit = 'kit' }
+    Import-Module (Join-Path $Packageroot $pgkit 'PSlib.psm1') -ErrorAction stop
+    #load Library dll
+    $cs = Add-PSDll
+    ##############################################
+
+    #Begin
+    $cs.Job_Start('WS', $AppName, $AppRelease, $LogFile, 'INSTALL')
+    $cs.Job_WriteLog("[Init]: Starting package: '" + $AppName + "' Release: '" + $AppRelease + "'")
+    if (!$cs.Sys_IsMinimumRequiredDiskspaceAvailable('c:', 1500)) { Exit-PSScript 3333 }
+    if ($global:DownloadPackage -and $InputObject) { Start-PSDownloadPackage }
+
+    $cs.Job_WriteLog("[Init]: `$PackageRoot:` '" + $Packageroot + "'")
+    $cs.Job_WriteLog("[Init]: `$AppName:` '" + $AppName + "'")
+    $cs.Job_WriteLog("[Init]: `$AppRelease:` '" + $AppRelease + "'")
+    $cs.Job_WriteLog("[Init]: `$LogFile:` '" + $LogFile + "'")
+    $cs.Job_WriteLog("[Init]: `$TempFolder:` '" + $TempFolder + "'")
+    $cs.Job_WriteLog("[Init]: `$DllPath:` '" + $DllPath + "'")
+    $cs.Job_WriteLog("[Init]: `$global:DownloadPackage`: '" + $global:DownloadPackage + "'")
+
+    $cs.Log_SectionHeader('Import custom modules', 'o')
+    Import-Module (Join-Path $Packageroot $pgkit 'Capa.PowerShell.Module.SDK.Authentication') -ErrorAction stop
+    Import-Module (Join-Path $Packageroot $pgkit 'Capa.PowerShell.Module.SDK.Unit') -ErrorAction stop
+
+    $cs.Log_SectionHeader('Initialize Capa SDK', 'o')
+    $cs.Job_WriteLog($CapaServer)
+    $cs.Job_WriteLog($Database)
     $oCMS = Initialize-CapaSDK -Server $CapaServer -Database $Database
+
     Set-UnitInactive -oCMS $oCMS
-} catch {
-    $Error[0]
-    $Script:ScriptFailed = 1
-}
 
-If ($Script:ScriptFailed -ne 0) {
-    $Text = "Script $ScriptName failed, check the log for more information on server $($env:COMPUTERNAME)"
-    Write-MBLogLine -ScriptPart $ScriptName -Text $Text -ForegroundColor Red
-    if ($Test -ne $true) {
-        #TODO: Send mail with error message
+    If ($Script:ScriptFailed -ne 0) {
+        $Text = "Script $AppName $AppRelease failed, check the log for more information on server $($env:COMPUTERNAME)"
+        $cs.Job_WriteLog($Text)
+        if ($Test -ne $true) {
+            #TODO: Send mail with error message
+        }
     }
-}
 
-Stop-MBScriptLogging
+    Exit-PSScript $Error
+
+} catch {
+    $line = $_.InvocationInfo.ScriptLineNumber
+    $cs.Job_WriteLog('*****************', "Something bad happend at line $($line): $($_.Exception.Message)")
+    Exit-PSScript $_.Exception.HResult
+}
