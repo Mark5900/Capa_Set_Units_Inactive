0cc39225b395fd4dc13e922fad393af1a89c8753
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2023-11-30 09:55:22 +0100

    update

--- Capa_Set_Units_Inactive.ps1
+++ Capa_Set_Units_Inactive.ps1
@@ -1,6 +1,5 @@
 Import-Module Capa.PowerShell.Module.SDK.Authentication
 Import-Module Capa.PowerShell.Module.SDK.Unit
-Import-Module Mark.Boennelykke
 <#
     .NOTES
     ===========================================================================
@@ -22,14 +21,15 @@ Import-Module Mark.Boennelykke
 ### PARAMETERS ###
 ##################
 # DO NOT CHANGE
-$ScriptName = 'Capa_Set_Units_Inactive'
+$ScriptName = 'Capa_SetUnitsInactive'
 $Script:ScriptFailed = 0
 # Change as needed
-$CapaServer = 'CISRVKURSUS'
-$Database = 'CapaInstaller'
+#TODO: $CapaServer = ''
+#TODO: $Database = ''
+#TODO: $InstanceManagementPoint = '2'
 $Script:LastRunDate = 90
 
-$Script:Test = $true # If $true no changes will be made
+$Script:Test = $false # If $true no changes will be made
 
 $Script:InactiveFolderStructure = "Inaktiv i mindst $Script:LastRunDate dage"
 $Script:InactiveActiveFolderStructure = "$Script:InactiveFolderStructure\Aktive igen" # If $null nothing happens else will move the units to the folder structure spiciefied
@@ -71,31 +71,31 @@ function Set-UnitInactive {
 
         #region Code
         if ($null -ne $Script:InactiveLastRunTimeFolderStructure -and $UnitLastExecuted -lt $Date -and $LastRunTime -gt $Date) {
-            Write-MBLogLine -ScriptPart $ScriptPart -Text "The unit $($Unit.Name) has not been executed for $Script:LastRunDate days but the last run time is $LastRunTime" -ForegroundColor Yellow
+            #TODO: ITCE-WriteLogLine -ScriptPart $ScriptPart -Text "The unit $($Unit.Name) has not been executed for $Script:LastRunDate days but the last run time is $LastRunTime" -ForegroundColor Yellow
 
             if ($Test -ne $true) {
-                [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer -FolderStructure $Script:InactiveLastRunTimeFolderStructure -CreateFolder true
+                [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.UUID -UnitType Computer -FolderStructure $Script:InactiveLastRunTimeFolderStructure -CreateFolder true
                 if ($Status -ne $true) {
-                    Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveLastRunTimeFolderStructure" -ForegroundColor Red
+                    #TODO: ITCE-WriteLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveLastRunTimeFolderStructure" -ForegroundColor Red
                     $Script:ScriptFailed = 1
                 }
-            }
+            } 
 
             $InactiveLastRunTimeCount++
         } elseif ($UnitLastExecuted -lt $Date) {
-            Write-MBLogLine -ScriptPart $ScriptPart -Text "Unit to set inactive: $($Unit.Name) - LastRunTime: $LastRunTime - UnitLastExecuted: $UnitLastExecuted" -ForegroundColor Red
+            #TODO: ITCE-WriteLogLine -ScriptPart $ScriptPart -Text "Unit to set inactive: $($Unit.Name) - LastRunTime: $LastRunTime - UnitLastExecuted: $UnitLastExecuted" -ForegroundColor Red
 
             if ($Test -ne $true) {
                 [bool]$Status = Set-CapaUnitStatus -CapaSDK $oCMS -UnitName $Unit.Name -Status Inactive
                 if ($Status -ne $true) {
-                    Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to set unit $($Unit.Name) to inactive" -ForegroundColor Red
+                    #TODO: ITCE-WriteLogLine -ScriptPart $ScriptPart -Text "Failed to set unit $($Unit.Name) to inactive" -ForegroundColor Red
                     $Script:ScriptFailed = 1
                 }
 
                 if ($UnitFolder -ne "$Script:InactiveFolderStructure\") {
-                    [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer -FolderStructure $Script:InactiveFolderStructure -CreateFolder true
+                    [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.UUID -UnitType Computer -FolderStructure $Script:InactiveFolderStructure -CreateFolder true
                     if ($Status -ne $true) {
-                        Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveFolderStructure" -ForegroundColor Red
+                        #TODO: ITCE-WriteLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveFolderStructure" -ForegroundColor Red
                         $Script:ScriptFailed = 1
                     }
                 }
@@ -103,12 +103,12 @@ function Set-UnitInactive {
 
             $InactiveCount++
         } elseif ($null -ne $Script:InactiveActiveFolderStructure -and $UnitFolder -eq "$Script:InactiveFolderStructure\" -and $Unit.Status -eq 'Active') {
-            Write-MBLogLine -ScriptPart $ScriptPart -Text "The unit $($Unit.Name) has become active again" -ForegroundColor Magenta
+            #TODO: ITCE-WriteLogLine -ScriptPart $ScriptPart -Text "The unit $($Unit.Name) has become active again" -ForegroundColor Magenta
 
             if ($Test -ne $true) {
-                [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.Name -UnitType Computer -FolderStructure $Script:InactiveActiveFolderStructure -CreateFolder true
+                [bool]$Status = Add-CapaUnitToFolder -CapaSDK $oCMS -UnitName $Unit.UUID -UnitType Computer -FolderStructure $Script:InactiveActiveFolderStructure -CreateFolder true
                 if ($Status -ne $true) {
-                    Write-MBLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveActiveFolderStructure" -ForegroundColor Red
+                    #TODO: ITCE-WriteLogLine -ScriptPart $ScriptPart -Text "Failed to add unit $($Unit.Name) to folder $Script:InactiveActiveFolderStructure" -ForegroundColor Red
                     $Script:ScriptFailed = 1
                 }
             }
@@ -118,29 +118,28 @@ function Set-UnitInactive {
         #endregion
     }
 
-    Write-MBLogLine -ScriptPart $ScriptPart -Text "Inactive: $InactiveCount - InactiveActive: $InactiveActiveCount - InactiveLastRunTime: $InactiveLastRunTimeCount"
+    #TODO: ITCE-WriteLogLine -ScriptPart $ScriptPart -Text "Inactive: $InactiveCount - InactiveActive: $InactiveActiveCount - InactiveLastRunTime: $InactiveLastRunTimeCount"
 }
 
 ##############
 ### SCRIPT ###
 ##############
 
-Start-MBScriptLogging -Path $PSScriptRoot -LogName $ScriptName -DeleteDaysOldLogs 30
+#TODO: ITCE-StartScriptLoggin -Path $PSScriptRoot -LogName $ScriptName -DeleteDaysOldLogs 30
 
 try {
-    $oCMS = Initialize-CapaSDK -Server $CapaServer -Database $Database
+    $oCMS = Initialize-CapaSDK -Server $CapaServer -Database $Database -InstanceManagementPoint $InstanceManagementPoint
     Set-UnitInactive -oCMS $oCMS
 } catch {
     $Error[0]
     $Script:ScriptFailed = 1
 }
 
-If ($Script:ScriptFailed -ne 0) {
-    $Text = "Script $ScriptName failed, check the log for more information on server $($env:COMPUTERNAME)"
-    Write-MBLogLine -ScriptPart $ScriptName -Text $Text -ForegroundColor Red
+If ($Global:ItceFejl -ne 0) {
+    $Text = "Script $ScriptName failed, check the log for more information on server $$($env:COMPUTERNAME)"
     if ($Test -ne $true) {
-        #TODO: Send mail with error message
+        #TODO: ITCE-SendMail -Subject $Text -Body $Text -To $MailTo
     }
 }
 
-Stop-MBScriptLogging
+#TODO: ITCE-StopScriptLoggin
--- README.md
+++ README.md
@@ -4,36 +4,27 @@ PowerShell script to set units inactive in CapaInstaller Console after x-days wh
 
 If the parameter `$Script:InactiveActiveFolderStructure` in not `$null` units who wil become active again will be moved to the folder specified by the parameter `$Script:InactiveActiveFolderStructure`.
 
-If the parameter `$Script:InactiveLastRunTimeFolderStructure` is not `$null`, units who have not executed for x-days but have newer run time than x-days will be moved to the folder specified by the parameter `$Script:InactiveLastRunTimeFolderStructure`. A little "good bug" is if you have multiple units with the same name, after x-days whre one of the units have not executed, both units will be moved to the folder specified by the parameter.
+If the parameter `$Script:InactiveLastRunTimeFolderStructure` is not `$null`, units who have not executed for x-days but have newer run time than x-days will be moved to the folder specified by the parameter `$Script:InactiveLastRunTimeFolderStructure`. A little "good bug" is if you have multiple units with the same name, after x-days where one of the units have not executed, both units will be moved to the folder specified by the parameter.
 
 ## Used modules 📦
 
  - [Capa.PowerShell.Module.SDK.Authentication](https://github.com/Mark5900/Capa.PowerShell.Module/tree/main/Modules/Capa.PowerShell.Module.SDK.Authentication)
  - [Capa.PowerShell.Module.SDK.Unit](https://github.com/Mark5900/Capa.PowerShell.Module/tree/main/Modules/Capa.PowerShell.Module.SDK.Unit)
- - [Mark.Boennelykke](https://github.com/Mark5900/Mark.Boennelykke)
 
 ### How to install the modules
 
-At the path `C:\Program Files\PowerShell\Modules` creat a folder named `Capa.PowerShell.Module.SDK.Authentication`, `Capa.PowerShell.Module.SDK.Unit` and `Mark.Boennelykke` and copy the .psm1 & .psd1 files into their respective folders.
+At the path `C:\Program Files\PowerShell\Modules` creat a folder named `Capa.PowerShell.Module.SDK.Authentication` and `Capa.PowerShell.Module.SDK.Unit`. Where you copy the .psm1 & .psd1 files into their respective folders.
+
+You can find [Capa.PowerShell.Module](https://github.com/Mark5900/Capa.PowerShell.Module) [here](https://github.com/Mark5900/Capa.PowerShell.Module).
 
 ## How to use 📋
 
 ### [Capa_Set_Units_Inactive.ps1](Capa_Set_Units_Inactive.ps1)
 
 Create a task in Windows Task Scheduler to run the script every day. The task need to run as an user with proper rights to the CapaInstaller Console.
+
 #### Important
 
 * NOTE: The script needs to be run as administrator.
 * NOTE: Remember to change parameters in the script to match your environment.
 * NOTE: Look throug the script for #TODO
-
-### [ServerTask_Capa_Set_Units_Inactive.ps1](ServerTask_Capa_Set_Units_Inactive.ps1)
-
-Create a new PowerPack packaget and copy the script into the install script. In the package kit create a folder for each of the following apps and add the .psd1 & .psm1 files to the folder.
-
- - [Capa.PowerShell.Module.SDK.Authentication](https://github.com/Mark5900/Capa.PowerShell.Module/tree/main/Modules/Capa.PowerShell.Module.SDK.Authentication)
- - [Capa.PowerShell.Module.SDK.Unit](https://github.com/Mark5900/Capa.PowerShell.Module/tree/main/Modules/Capa.PowerShell.Module.SDK.Unit)
-
-#### Important
-- NOTE: Remember to change parameters in the script to match your environment.
-- NOTE: Look throug the script for #TODO
